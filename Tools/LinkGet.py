42da5bbd5de96256dc70efd1104b0f4b336d8a1c
Light Beacon <2682645990@qq.com>
2024-02-16 01:03:23 +0800

    remove tool

--- Tools/LinkGet.py
+++
@@ -1,9 +0,0 @@
-import json
-def trans(froms,key,dest):
-    mapping = {}
-    for file in froms:
-        with open(file,'r',encoding='utf-8') as f:
-            for item in json.load(f)['cards']:
-                mapping.update({item['name']:item[key]})
-    with open(dest,'w',encoding='utf-8') as f:            
-        json.dump(mapping,f,ensure_ascii=False)
