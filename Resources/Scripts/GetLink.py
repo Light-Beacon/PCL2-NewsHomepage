59a1412a24749dbbb9cb1c87c1467e2680b6a8eb
Light Beacon <2682645990@qq.com>
2024-02-17 02:24:56 +0800

    fix bug

--- Resources/Scripts/GetLink.py
+++ Resources/Scripts/GetLink.py
@@ -1,7 +1,9 @@
 from .Debug import LogWarning, LogInfo
 def script(card,arg,res):
-    name = card['version-id']
+    name:str = card['version-id']
     data = res.data[f'{arg[1]}Link']
+    if '${' in name:
+        raise KeyError()
     if arg[1] == 'Mcbbs' and name not in data:
         LogInfo(f'尝试获取MCBBS链接 {name}，但未找到，由于 MCBBS 关站，返回空值以抑制 Warning')
         return ''
