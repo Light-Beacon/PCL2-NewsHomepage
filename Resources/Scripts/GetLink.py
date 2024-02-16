6cdd7c088731972568a3ea32dfec09cabfae188d
Light Beacon <2682645990@qq.com>
2024-02-17 02:14:06 +0800

    suppress warnings due to mcbbs

--- Resources/Scripts/GetLink.py
+++ Resources/Scripts/GetLink.py
@@ -1,3 +1,8 @@
+from .Debug import LogWarning, LogInfo
 def script(card,arg,res):
     name = card['version-id']
-    return res.data[f'{arg[1]}Link'][name]
+    data = res.data[f'{arg[1]}Link']
+    if arg[1] == 'Mcbbs' and name not in data:
+        LogInfo(f'尝试获取MCBBS链接 {name}，但未找到，由于 MCBBS 关站，返回空值以抑制 Warning')
+        return ''
+    return data[name]
