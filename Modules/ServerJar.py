55ad9738dae5580e689ef51fefe0d6bc814211c3
Light Beacon <2682645990@qq.com>
2024-07-28 23:52:04 +0800

    fix

--- Modules/ServerJar.py
+++ Modules/ServerJar.py
@@ -1,7 +1,7 @@
 from Interfaces import invoke,require,script
 
 mcv = require('MinecraftVersions') # 需求前置 MinecraftVersions
-get_server_jar = mcv.get_server_jar()
+get_server_jar = mcv.get_server_jar
 
 @script('ServerJar')
 def script(card,**_):
