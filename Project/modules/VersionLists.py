8feb48127c07d30689a9742109acf281f9a859b6
Nattiden <2682645990@qq.com>
2024-11-10 11:00:33 +0800

    fix

--- Project/modules/VersionLists.py
+++ Project/modules/VersionLists.py
@@ -8,32 +8,33 @@ ID_LIST = [version.get('id') for version in FULL_VERSIONS]
 
 @script('VersionArchiveList')
 def version_achive_list(cat_name,env,card,**_):
-    cat_name = format_code(code = cat_name,card=card,env=env)
+    cat_name = format_code(code = cat_name,data=card,env=env)
     cards = list(filter(lambda card:isinstance(card.get('cats'),list)
            and cat_name in card.get('cats'), env.get('project').get_all_card()))
     code = '<StackPanel Margin="8,2,8,15">'
     res = env.get('project').resources
     cards.sort(key=lambda card:ID_LIST.index(format_code(card['version-id'],
-                                                         card=card,project=proj)))
+                                                         data=card,env=env)))
     if len(cards) > 0 and cards[0]['version-type-id'] not in ['Release','April-Fools']:
         code += res.components['VersionLinks/Future']
     for vercard in cards:
         if vercard.get('lack'):
             code += format_code(code = res.components['VersionLinks/Lack'],
-                            card=vercard,project=proj)
+                            data=vercard,env=env)
         else:
             code += format_code(code = res.components['VersionLinks/Common'],
-                            card=vercard,project=proj)
+                            data=vercard,env=env)
     code += '</StackPanel>'
     return code
 
 @script('VersionLatestList')
-def version_latest_list(proj:Project,card,**_):
-    res = proj.resources
+def version_latest_list(env,**_):
+    proj = env.get('project')
+    components = env.get('components')
     code = ''
     for version_type in ['release','snapshot']:
         if latest_version := mcv.get_latest(version_type):
             vercard = proj.base_library.get_card(latest_version,False)
-            code += format_code(code = res.components['VersionLinks/Latest'],
-                            card=vercard,project=proj)
+            code += format_code(code = components['VersionLinks/Latest'],
+                            data=vercard,env=env)
     return code
