2c5787807dfaa27c7ce179ddc9fa40c6f91f2e53
Nattiden <2682645990@qq.com>
2024-11-14 09:46:37 +0800

    fix bugs

--- Project/modules/CheckDomainSafety.py
+++ Project/modules/CheckDomainSafety.py
@@ -3,6 +3,7 @@ from homepagebuilder.interfaces.Events import on, ResultOverride
 
 @on('page.generate.start')
 def script(_page,env):
-    if mod_domain := env.get('setter').override.get('domain'):
-        if mod_domain not in config('safe_domains',['news.bugjump.net']):
-            raise ResultOverride('')
+    if env.get('setter'):
+        if mod_domain := env.get('setter').override.get('domain'):
+            if mod_domain not in config('safe_domains',['news.bugjump.net']):
+                raise ResultOverride('')
