f218126d004c4f2d7a7e1d70d0869f3ec6fd7681
Nattiden <2682645990@qq.com>
2025-12-18 19:40:24 +0800

    fix offical link

--- Project/modules/offical_link.py
+++ Project/modules/offical_link.py
@@ -7,6 +7,7 @@ from homepagebuilder.interfaces import script
 from homepagebuilder.core.formatter import format_code
 
 SNAPSHOT_PATTERN = re.compile(r'^[0-9]{2}[w|W][0-9]{2}[A-Fa-f]$')
+NEW_SNAPSHOT_PATTERN = re.compile(r'^[0-9]{2}(\.[0-9]+)*-snapshot-[0-9]+$')
 PRE_RELEASE_PATTERN = re.compile(r'^.*-pre[0-9]+$')
 RELEASE_CANDIDATE_PATTERN = re.compile(r'^.*-rc[0-9]+$')
 RELEASE_PATTERN = re.compile(r'^1\.[0-9]+(\.[0-9]+)?$')
@@ -15,6 +16,8 @@ def gen_official_link(vid:str):
     '''生成官网链接'''
     if re.match(SNAPSHOT_PATTERN,vid):
         return f'https://minecraft.net/article/minecraft-snapshot-{vid.lower()}'
+    if re.match(NEW_SNAPSHOT_PATTERN,vid):
+        return f'https://minecraft.net/article/minecraft-{vid.replace(".","-")}'
     if re.match(PRE_RELEASE_PATTERN,vid):
         version,sub=vid.split('-pre')
         return f'https://minecraft.net/article/minecraft-{version.replace(".","-")}-pre-release-{sub}'
